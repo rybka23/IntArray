5d7669d2cc64e0fe216e798ca838c011be8cc7f4
rybka23 <166043580+rybka23@users.noreply.github.com>
2024-04-05 21:36:24 +0500

    Delete IntArrayRC directory

--- IntArrayRC/IntArrayRC.cpp
+++
@@ -1,9 +0,0 @@
-#include "IntArrayRC.h"
-#include <cassert>
-#include <stdexcept>
-
-void IntArrayRC::check(int index) {
-    //assert(index >= 0 && index < _size);
-    if (index < 0 || index >= _size)
-        throw std::invalid_argument("Index out of range");
-}
